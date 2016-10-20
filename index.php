45e72aa8596b790f7ad6c39ec3fd7bf1f321dc9e
Sergio Fernández <sergio@wikier.org>
2016-10-20 14:05:57 +0200

    fixed test analysis name

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ require_once("src/bootstrap.php");
 // Currently the DefaultCredentials parameter is not used
 
 $key = "QVBU1skUrDBlapFOeHZZHa3uqs1fI3vwb67d6405"; //FIXME
+$analysis = "php-sdk";
 
 $credentials = new RedLink\Credential\SecureCredentials($key);
 $credentials->setSSLCertificates(__DIR__.DIRECTORY_SEPARATOR.'redlink-CA.pem');
 
 $analysisClient = \RedLink\RedLinkFactory::getInstance()->createAnalysisClient($key);
-$request = \RedLink\Analysis\AnalysisRequestBuilder::builder()->setAnalysis("alfresco")->setContent("Paris is the capital of France")->setOutputFormat("json")->build();
+$request = \RedLink\Analysis\AnalysisRequestBuilder::builder()->setAnalysis($analysis)->setContent("Paris is the capital of France")->setOutputFormat("json")->build();
 //echo $analysisClient->enhanceRaw($request);
 var_dump($analysisClient->enhance($request, true));
 
--- tests/RedLink/Tests/Analysis/RedLinkAnalysisImplTest.php
+++ tests/RedLink/Tests/Analysis/RedLinkAnalysisImplTest.php
@@ -13,11 +13,12 @@ class RedLinkAnalysisImplTest extends \PHPUnit_Framework_TestCase
     private $redlinkAnalysis;
     
     const TEXT_TO_ENHANCE = "Paris is the capital of France";
+    const API_KEY = "QVBU1skUrDBlapFOeHZZHa3uqs1fI3vwb67d6405"; //FIXME
+    const ANALYSIS_NAME = "php-sdk";
     
-    protected function setUp()
-    {
+    protected function setUp() {
         parent::setUp();
-        $credentials = new \RedLink\Credential\SecureCredentials("QVBU1skUrDBlapFOeHZZHa3uqs1fI3vwb67d6405"); //FIXME
+        $credentials = new \RedLink\Credential\SecureCredentials(self::API_KEY);
         $credentials->setSSLCertificates(REDLINK_ROOT_PATH . DIRECTORY_SEPARATOR . 'redlink-CA.pem');
         $this->redlinkAnalysis = new \RedLink\Analysis\RedLinkAnalysisImpl($credentials);
     }
@@ -26,7 +27,7 @@ class RedLinkAnalysisImplTest extends \PHPUnit_Framework_TestCase
      * @covers \RedLink\Analysis\RedLinkAnalysisImpl\enhance
      */
     public function testEnhance() {
-        $analysisRequest = \RedLink\Analysis\AnalysisRequestBuilder::builder()->setAnalysis("alfresco")->setContent(self::TEXT_TO_ENHANCE)->build();
+        $analysisRequest = \RedLink\Analysis\AnalysisRequestBuilder::builder()->setAnalysis(self::ANALYSIS_NAME)->setContent(self::TEXT_TO_ENHANCE)->build();
         $enhancements = $this->redlinkAnalysis->enhance($analysisRequest);
         $this->assertNotNull($enhancements);
     }
